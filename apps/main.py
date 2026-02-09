922af2a223c9d9422e5d29b19e8344dad13aba44
m_tokuda <m1tkd23@icloud.com>
2026-02-09 11:48:32 +0900

    Initial commit from template

---
+++ apps/main.py
@@ -0,0 +1,9 @@
+﻿"""
+アプリケーションのエントリポイント。
+CLI / GUI / Web いずれの場合も、このファイルは"薄く"保つ。
+"""
+from src.hypermill_nctools_inventory_exporter.core import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_smoke.py
@@ -0,0 +1,8 @@
+﻿"""
+最低限のスモークテスト。
+起動できることだけを確認する。
+"""
+
+
+def test_import():
+    import src.hypermill_nctools_inventory_exporter.core
